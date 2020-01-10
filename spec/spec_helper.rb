12693355572eef8e2326534f6f4fe3c09ed6b8d6
eugene <ezvy@yandex.ru>
2020-01-10 15:11:39 +0300

    Fix rubocop warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,7 @@ require 'active_record'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].sort.each { |f| require f }
 
 RSpec.configure do |config|
 end
